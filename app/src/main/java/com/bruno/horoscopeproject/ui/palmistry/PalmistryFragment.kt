79075457c85298f5d40a27e9753e4ee38f707c4a
brunopodesta <89876387+brunopodesta@users.noreply.github.com>
2025-04-23 13:08:11 +0200

    show preview and palm image

--- app/src/main/java/com/bruno/horoscopeproject/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/bruno/horoscopeproject/ui/palmistry/PalmistryFragment.kt
@@ -1,15 +1,18 @@
 package com.bruno.horoscopeproject.ui.palmistry
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker
-import com.bruno.horoscopeproject.Manifest
-import com.bruno.horoscopeproject.R
+import androidx.fragment.app.Fragment
 import com.bruno.horoscopeproject.databinding.FragmentPalmistryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -26,7 +29,7 @@ class PalmistryFragment : Fragment() {
     private val requestPermissionLauncher =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
             if (isGranted) {
-
+                startCamera()
             } else {
                 Toast.makeText(requireContext(), "Debe aceptar los permisos para poder continuar", Toast.LENGTH_LONG).show()
             }
@@ -35,10 +38,34 @@ class PalmistryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         if (checkCameraPermission()) {
-
+            startCamera()
         } else {
             requestPermissionLauncher.launch(CAMERA_PERMISSION)
         }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture.addListener({
+            val cameraProvider : ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+              cameraProvider.unbindAll()
+
+              cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            } catch (e : Exception) {
+                Log.e("Horoscope Cam", "Error: ${e.message}")
+            }
+
+        }, ContextCompat.getMainExecutor((requireContext())))
+
     }
 
     private fun checkCameraPermission(): Boolean {
